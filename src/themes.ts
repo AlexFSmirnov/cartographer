0866fc4046eec1181753a0ba54486356c1919c59
Alexander Smirnov <alex.f.smirnov@gmail.com>
2022-08-23 00:54:32 +0200

    Updated light color scheme

--- src/themes.ts
+++ src/themes.ts
@@ -9,5 +9,11 @@ export const darkTheme = createTheme({
 export const lightTheme = createTheme({
     palette: {
         mode: 'light',
+        primary: {
+            contrastText: 'rgba(0, 0, 0, 0.87)',
+            dark: '#2d71a8',
+            light: '#90caf9',
+            main: '#42a5f5',
+        },
     },
 });
